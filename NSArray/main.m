b87e4217e54f554480a2f753e316641dece42124
noprom <tyee.noprom@qq.com>
2015-06-21 21:42:34 +0800

    数组长度

--- NSArray/main.m
+++ NSArray/main.m
@@ -10,8 +10,15 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        // OC 数组可以存储不同类型的对象,且只能存储对象
+        // 内存空间是连续的,保存对象的时候，存放的是对象的指针
+        
+        NSArray *array1 = [[NSArray alloc] initWithObjects:@"1", @"2", @"3", @"4", @"5", nil];
+        int count = (int) array1.count; // 数组长度
+        NSLog(@"count = %d", count);
+        
+        
     }
     return 0;
 }
