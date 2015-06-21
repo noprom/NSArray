27e387452a824ac39087d201be5af29ef702e6c0
noprom <tyee.noprom@qq.com>
2015-06-21 21:50:19 +0800

    得到数组中最后一个元素

--- NSArray/main.m
+++ NSArray/main.m
@@ -15,8 +15,21 @@ int main(int argc, const char * argv[]) {
         // 内存空间是连续的,保存对象的时候，存放的是对象的指针
         
         NSArray *array1 = [[NSArray alloc] initWithObjects:@"1", @"2", @"3", @"4", @"5", nil];
-        int count = (int) array1.count; // 数组长度
+        // 数组长度
+        int count = (int) array1.count;
         NSLog(@"count = %d", count);
+        
+        // 判断数组中是否包含特定对象
+        BOOL isHave = [array1 containsObject:@"2"];
+        if(isHave){
+            NSLog(@"存在！");
+        }else{
+            NSLog(@"不存在!");
+        }
+        
+        // 得到数组中最后一个元素
+        NSString *last = [array1 lastObject];
+        NSLog(@"Last obect = %@",last);
         
         
     }
